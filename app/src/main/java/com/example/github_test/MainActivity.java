0e8a2e3c520f67b78e9b67a2e41bffe80e2568d1
Prl-1234 <priyal0917@gmail.com>
2020-12-26 15:35:55 +0530

    17 commit branch5

--- app/src/main/java/com/example/github_test/MainActivity.java
+++ app/src/main/java/com/example/github_test/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView ree=(TextView) findViewById(R.id.one);
+        TextView ret=(TextView) findViewById(R.id.last);
 
 
     }
